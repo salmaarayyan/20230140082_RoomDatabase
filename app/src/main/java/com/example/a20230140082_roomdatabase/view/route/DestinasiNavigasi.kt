c0d249901faec5d73cb1bce814818a5108bbc8f1
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:25:26 +0700

    Menambahkan penjelasan route dan titleRes di DestinasiNavigasi

--- app/src/main/java/com/example/a20230140082_roomdatabase/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/view/route/DestinasiNavigasi.kt
@@ -1,6 +1,13 @@
 package com.example.a20230140082_roomdatabase.view.route
 
 interface DestinasiNavigasi {
+    /**
+     * nama unik untuk menentukan jalur untuk composable
+     */
     val route: String
+
+    /**
+     * string resource id yang berisi judul yang akan ditampilkan di layar halaman
+     */
     val titleRes: Int
 }
