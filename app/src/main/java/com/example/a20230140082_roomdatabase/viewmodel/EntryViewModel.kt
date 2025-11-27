c5973f1462d3e664eea7f6f8e41fd8cf9a6468fc
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:46:16 +0700

    Menambahkan extension function toSiswa untuk DetailSiswa

--- app/src/main/java/com/example/a20230140082_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/viewmodel/EntryViewModel.kt
@@ -37,3 +37,11 @@ data class DetailSiswa(
     val alamat: String = "",
     val telpon: String = "",
 )
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
