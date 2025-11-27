dde891aa734f3313b142654ac4bc28cfb4f2edb1
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:51:12 +0700

    Menambahkan extension function toDetailSiswa untuk Siswa

--- app/src/main/java/com/example/a20230140082_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/viewmodel/EntryViewModel.kt
@@ -1,7 +1,8 @@
 package com.example.a20230140082_roomdatabase.viewmodel
 
-import android.app.PictureInPictureUiState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.a20230140082_roomdatabase.repositori.RepositoriSiswa
 import com.example.a20230140082_roomdatabase.room.Siswa
@@ -28,17 +29,26 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() 
     }
 }
 
-data class uiStateSiswa(
+/**
+ * mewakili status UI saat ini
+ */
+
+data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
 
 data class DetailSiswa(
     val id: Int = 0,
-    val nama: String ="",
+    val nama: String = "",
     val alamat: String = "",
     val telpon: String = "",
 )
+
+/**
+ * Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya
+ */
+
 
 fun DetailSiswa.toSiswa(): Siswa = Siswa(
     id = id,
@@ -47,8 +57,16 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     telpon = telpon
 )
 
-fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSsiswa = uiStateSiswa(
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false)
+        : UIStateSiswa = UIStateSiswa(
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
 
