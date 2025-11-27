7e9daede23926a048b207bfa93de7ef239a84dc7
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:05:51 +0700

    Menambahkan class DatabaseSiswa untuk Room

---
+++ app/src/main/java/com/example/a20230140082_roomdatabase/room/DatabaseSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.a20230140082_roomdatabase.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun siswaDao() : SiswaDao
+
+
+}
