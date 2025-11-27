c853969661e84aeff5af0e52e7d3cd6dbeb578c7
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:45:17 +0700

    Menambahkan data class DetailSiswa

--- app/src/main/java/com/example/a20230140082_roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/viewmodel/EntryViewModel.kt
@@ -30,3 +30,10 @@ data class uiStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
     val isEntryValid: Boolean = false
 )
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String ="",
+    val alamat: String = "",
+    val telpon: String = "",
+)
