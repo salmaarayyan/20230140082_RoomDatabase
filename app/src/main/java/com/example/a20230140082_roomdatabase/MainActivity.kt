224449196a05fcd681d601e8c46ee529c9f65894
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 14:26:30 +0700

    Membungkus SiswaApp dengan Scaffold dan padding

--- app/src/main/java/com/example/a20230140082_roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a20230140082_roomdatabase.ui.theme._20230140082_RoomDatabaseTheme
+import com.example.a20230140082_roomdatabase.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             _20230140082_RoomDatabaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
