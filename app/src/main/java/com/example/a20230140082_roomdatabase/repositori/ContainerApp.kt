c47d4d658edee78fb958c46294eb61fcde9b7db5
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:18:16 +0700

    Menambahkan class ContainerDataApp untuk menyediakan RepositoriSiswa

--- app/src/main/java/com/example/a20230140082_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,16 @@
 package com.example.a20230140082_roomdatabase.repositori
 
+import android.content.Context
+import com.example.a20230140082_roomdatabase.room.DatabaseSiswa
+
 interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
 }
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+        }
