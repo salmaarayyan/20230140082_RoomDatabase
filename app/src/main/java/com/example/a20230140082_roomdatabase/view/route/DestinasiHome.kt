2c281010863b6875e7a109c503dc7150d196db68
salmaarayyan <salmaarayyan@gmail.com>
2025-11-27 13:28:39 +0700

    Menambahkan object DestinasiHome sebagai DestinasiNavigasi

---
+++ app/src/main/java/com/example/a20230140082_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a20230140082_roomdatabase.view.route
+
+import com.example.a20230140082_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
