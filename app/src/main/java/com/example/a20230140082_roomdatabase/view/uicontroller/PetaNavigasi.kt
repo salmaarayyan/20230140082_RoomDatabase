8df45b3139c2ee4421fc93aaed1e92bec1747b9a
salmaarayyan <salmaarayyan@gmail.com>
2025-12-04 12:14:01 +0700

    Menambahkan navigasi ke DetailSiswaScreen

--- app/src/main/java/com/example/a20230140082_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.a20230140082_roomdatabase.view.DetailSiswaScreen
 import com.example.a20230140082_roomdatabase.view.EntrySiswaScreen
 import com.example.a20230140082_roomdatabase.view.HomeScreen
+import com.example.a20230140082_roomdatabase.view.route.DestinasiDetailSiswa
+import com.example.a20230140082_roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.a20230140082_roomdatabase.view.route.DestinasiEntry
 import com.example.a20230140082_roomdatabase.view.route.DestinasiHome
 
@@ -29,11 +34,23 @@ fun HostNavigasi(
         composable(DestinasiHome.route){
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetailSiswa.route}/${it}")
+                }
             )
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.popBackStack()})
         }
 
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg) {
+            type = NavType.IntType
+        })
+        ){
+            DetailSiswaScreen(
+                //navigateToEditItem = { navController.navigate("${DestinasiEditSiswa.route}/$it") },
+                navigateBack = { navController.navigateUp() })
+        }
     }
 }
