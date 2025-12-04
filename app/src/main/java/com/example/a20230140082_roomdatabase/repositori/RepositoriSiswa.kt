9e5efa1313ee3584c193d37bf6a7ae975b0cb80d
salmaarayyan <salmaarayyan@gmail.com>
2025-12-04 13:57:07 +0700

    Menambahkan fungsi `updateSiswa` pada repositori

--- app/src/main/java/com/example/a20230140082_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a20230140082_roomdatabase/repositori/RepositoriSiswa.kt
@@ -9,14 +9,15 @@ interface RepositoriSiswa {
     suspend fun insertSiswa(siswa: Siswa)
     fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
 ): RepositoriSiswa{
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
